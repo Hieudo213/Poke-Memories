dfb0eb281449e4047899894f6dd8e65ede1eafe2
Hieudo213 <hieudoo213@gmail.com>
2024-09-10 15:09:33 +0700

    mini game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="setting.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
+  <copy-right />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import CopyRight from "./components/CopyRight.vue";
+import { shuffled } from "./utils/array";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { MainScreen, InteractScreen, ResultScreen, CopyRight },
+  data() {
+    return {
+      setting: {
+        totalBlock: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      this.setting.totalBlock = config.totalBlocks;
+      const firstCards = Array.from(
+        { length: this.setting.totalBlock / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+      // data ready
+      this.setting.cardsContext = shuffled(shuffled(shuffled(shuffled(cards))));
+      this.setting.startedAt = new Date().getTime();
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      // get timer
+      this.timer = new Date().getTime() - this.setting.startedAt;
+      // switch to result component
+      this.statusMatch = "result";
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
